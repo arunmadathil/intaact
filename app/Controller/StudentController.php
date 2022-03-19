7456feea8705ce6eb9b7944c90b2685d85297971
arunmadathil <arunmadathil005@yahoo.com>
2022-03-19 21:13:07 +0530

    validation and model

--- app/Controller/StudentController.php
+++ app/Controller/StudentController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use App\Core\Requests;
-use App\Core\RouteServiceProvider;
+use App\Model\Student;
 
 class StudentController extends Controller
 {
 
     public function __construct()
     {
+
     }
 
     public function index()
@@ -17,20 +18,44 @@ class StudentController extends Controller
         return $this->view('contact');
     }
 
-    public function create()
+    public function create(Requests $request)
     {
+        $errors = $request->validate([
+            
+            'first_name' => 'required | string | min:2 | max: 20',
+            'last_name' => 'required | string | max: 20',
+            'phone_no' => 'required | string | min:2 | max: 10',
+            'dob' => 'required | date_format:Y-m-d ',
 
-        return "Form submited!";
+        ]);
+        
+        if(!empty($errors)){
+
+            return $this->view('contact',['errors' => $errors]);
+
+        }
+
+        $student = new Student;
+        $student->first_name = $request->first_name;
+        $student->last_name = $request->last_name;
+        $student->dob = $request->dob;
+        $student->contact_no = $request->contact_no;
+        $student->create();
+       
     }
 
     public function show()
     {
 
         $parmas = [
-            'first_name' => 'Arun',
-            'last_name' => 'M',
-            'email' => 'arun@yahoo.com'
+
+            'first_name' => 'Arun | string | max:20 | min: 2',
+
+            'last_name' => 'M | string',
+
+            'email' => 'arun@yahoo.com | email | max:50 | min:1'
         ];
+
         return $this->view('view_contact',$parmas);
     }
 }
